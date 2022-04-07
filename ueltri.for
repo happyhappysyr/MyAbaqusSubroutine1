aca88dfa2692be46b0e7a433ba4ef1635af7b2c8
prashanthvsdvn <prashanthvsdvn@gmail.com>
2022-04-07 14:04:35 -0500

    initial commit

---
+++ ueltri.for
@@ -0,0 +1,79 @@
+      SUBROUTINE UEL(RHS,AMATRX,SVARS,ENERGY,NDOFEL,NRHS,NSVARS,
+     1 PROPS,NPROPS,COORDS,MCRD,NNODE,U,DU,V,A,JTYPE,TIME,DTIME,
+     2 KSTEP,KINC,JELEM,PARAMS,NDLOAD,JDLTYP,ADLMAG,PREDEF,NPREDF,
+     3 LFLAGS,MLVARX,DDLMAG,MDLOAD,PNEWDT,JPROPS,NJPROP,PERIOD)
+C
+      INCLUDE 'ABA_PARAM.INC'
+C
+      DIMENSION RHS(MLVARX,*),AMATRX(NDOFEL,NDOFEL),PROPS(*),
+     1 SVARS(*),ENERGY(8),COORDS(MCRD,NNODE),U(NDOFEL),
+     2 DU(MLVARX,*),V(NDOFEL),A(NDOFEL),TIME(2),PARAMS(*),
+     3 JDLTYP(MDLOAD,*),ADLMAG(MDLOAD,*),DDLMAG(MDLOAD,*),
+     4 PREDEF(2,NPREDF,NNODE),LFLAGS(*),JPROPS(*)
+
+
+C      Checking for correct element types
+      IF(NNODE.EQ.3) THEN
+          WRITE(*,*) "ELEMENT HAS CORRECT NUMBER OF NODES (",NNODE,")"
+        ELSE
+          WRITE(*,*) "ELEMENT HAS WRONG NUMBER OF NODES ",NNODE,")"
+C      Assiging the Material Coordinates from COORD
+      E = PROPS(1)
+      PR = PROPS(2)
+      THICKNESS = PROPS(3)
+      AREA = PROPS(4)
+C      Assiging the Material Coordinates from COORD
+      X_I = COORDS(1,1)
+      X_J = COORDS(2,1)
+      X_M = COORDS(3,1)
+      Y_I = COORDS(1,2)
+      Y_J = COORDS(2,2)
+      Y_M = COORDS(3,2)
+
+      BETA_I = Y_J - Y_M
+      BETA_J = Y_M - Y_I
+      BETA_M = Y_I - Y_J
+C
+      GAMMA_I = X_M - X_J
+      GAMMA_J = X_I - X_M
+      GAMMA_M = X_J - X_I
+
+C     Assembling B Matrix
+      REAL BMATRIX(6,3)
+      BMATRIX(1,1) = BETA_I/(2*AREA)
+      BMATRIX(2,1) = 0
+      BMATRIX(3,1) = BETA_J/(2*AREA)
+      BMATRIX(4,1) = 0
+      BMATRIX(5,1) = BETA_M/(2*AREA)
+      BMATRIX(6,1) = 0
+      BMATRIX(1,2) = 0
+      BMATRIX(2,2) = GAMMA_I/(2*AREA)
+      BMATRIX(3,2) = 0
+      BMATRIX(4,2) = GAMMA_J/(2*AREA)
+      BMATRIX(5,2) = 0
+      BMATRIX(6,2) = GAMMA_M/(2*AREA)
+      BMATRIX(1,3) = GAMMA_I/(2*AREA)
+      BMATRIX(2,3) = BETA_I/(2*AREA)
+      BMATRIX(3,3) = GAMMA_J/(2*AREA)
+      BMATRIX(4,3) = BETA_J/(2*AREA)
+      BMATRIX(5,3) = GAMMA_M/(2*AREA)
+      BMATRIX(6,3) = BETA_M/(2*AREA)
+
+C       Assembling [D] Matrix
+      REAL DMATRIX(3,3)
+      DMATRIX(1,1) = (E/(1-PR**2))*(1)
+      DMATRIX(2,1) = (E/(1-PR**2))*(PR)
+      DMATRIX(3,1) = 0
+      DMATRIX(1,2) = (E/(1-PR**2))*(PR)
+      DMATRIX(2,2) = (E/(1-PR**2))*(1)
+      DMATRIX(3,2) = 0
+      DMATRIX(1,3) = 0
+      DMATRIX(2,3) = 0
+      DMATRIX(3,3) = (E/(1-PR**2))*((1-2*PR)/2)
+
+C      MATRIX MULTIPLICATION FOR STIFFNESS MATRIX
+      BTRANS = TRANSPOSE(BMATRIX)*(THICKNESS*AREA)
+      KMATRIX = MATMUL(MATMUL(BTRANS,DMATRIX),BMATRIX)
+
+      RETURN
+      END
