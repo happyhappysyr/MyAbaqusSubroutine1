6ff5cde22875e6ee9652508ed23df5f5ad407b66
Prashanth Vasudevan <prashanthvsdvn@gmail.com>
2022-04-18 15:52:31 -0500

    Fixed Matrix Sizes and assignment statements. Fortran compiling now

--- ueltri.for
+++ ueltri.for
@@ -10,6 +10,9 @@ C
      2 DU(MLVARX,*),V(NDOFEL),A(NDOFEL),TIME(2),PARAMS(*),
      3 JDLTYP(MDLOAD,*),ADLMAG(MDLOAD,*),DDLMAG(MDLOAD,*),
      4 PREDEF(2,NPREDF,NNODE),LFLAGS(*),JPROPS(*)
+      REAL BMATRIX(3,6)
+      REAL BTRANS(6,3)
+      REAL DMATRIX(3,3)
 
 
 C      Checking for correct element types
@@ -17,6 +20,7 @@ C      Checking for correct element types
           WRITE(*,*) "ELEMENT HAS CORRECT NUMBER OF NODES (",NNODE,")"
         ELSE
           WRITE(*,*) "ELEMENT HAS WRONG NUMBER OF NODES ",NNODE,")"
+        END IF
 C      Assiging the Material Coordinates from COORD
       E = PROPS(1)
       PR = PROPS(2)
@@ -39,28 +43,26 @@ C
       GAMMA_M = X_J - X_I
 
 C     Assembling B Matrix
-      REAL BMATRIX(6,3)
       BMATRIX(1,1) = BETA_I/(2*AREA)
-      BMATRIX(2,1) = 0
-      BMATRIX(3,1) = BETA_J/(2*AREA)
-      BMATRIX(4,1) = 0
-      BMATRIX(5,1) = BETA_M/(2*AREA)
-      BMATRIX(6,1) = 0
       BMATRIX(1,2) = 0
+      BMATRIX(1,3) = BETA_J/(2*AREA)
+      BMATRIX(1,4) = 0
+      BMATRIX(1,5) = BETA_M/(2*AREA)
+      BMATRIX(1,6) = 0
+      BMATRIX(2,1) = 0
       BMATRIX(2,2) = GAMMA_I/(2*AREA)
-      BMATRIX(3,2) = 0
-      BMATRIX(4,2) = GAMMA_J/(2*AREA)
-      BMATRIX(5,2) = 0
-      BMATRIX(6,2) = GAMMA_M/(2*AREA)
-      BMATRIX(1,3) = GAMMA_I/(2*AREA)
-      BMATRIX(2,3) = BETA_I/(2*AREA)
+      BMATRIX(2,3) = 0
+      BMATRIX(2,4) = GAMMA_J/(2*AREA)
+      BMATRIX(2,5) = 0
+      BMATRIX(2,6) = GAMMA_M/(2*AREA)
+      BMATRIX(3,1) = GAMMA_I/(2*AREA)
+      BMATRIX(3,2) = BETA_I/(2*AREA)
       BMATRIX(3,3) = GAMMA_J/(2*AREA)
-      BMATRIX(4,3) = BETA_J/(2*AREA)
-      BMATRIX(5,3) = GAMMA_M/(2*AREA)
-      BMATRIX(6,3) = BETA_M/(2*AREA)
+      BMATRIX(3,4) = BETA_J/(2*AREA)
+      BMATRIX(3,5) = GAMMA_M/(2*AREA)
+      BMATRIX(3,6) = BETA_M/(2*AREA)
 
 C       Assembling [D] Matrix
-      REAL DMATRIX(3,3)
       DMATRIX(1,1) = (E/(1-PR**2))*(1)
       DMATRIX(2,1) = (E/(1-PR**2))*(PR)
       DMATRIX(3,1) = 0
@@ -72,8 +74,9 @@ C       Assembling [D] Matrix
       DMATRIX(3,3) = (E/(1-PR**2))*((1-2*PR)/2)
 
 C      MATRIX MULTIPLICATION FOR STIFFNESS MATRIX
-      BTRANS = TRANSPOSE(BMATRIX)*(THICKNESS*AREA)
-      KMATRIX = MATMUL(MATMUL(BTRANS,DMATRIX),BMATRIX)
+      BTRANS = TRANSPOSE(BMATRIX)
+c      BTRANS = TRANSPOSE(BMATRIX)*(THICKNESS*AREA)
+      AMATRX = MATMUL(MATMUL(BTRANS,DMATRIX),BMATRIX)
 
       RETURN
       END
